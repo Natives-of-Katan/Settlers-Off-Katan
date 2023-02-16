e7ff8fa150142618c810f18071211cd7fcefa9e6
Jonathan Espinosa <jonespinosa89@gmail.com>
2023-02-15 23:20:28 -0500

    post requests from createAccount.js now functional, next step handling response

--- client/src/CreateAccount.js
+++ client/src/CreateAccount.js
@@ -1,46 +1,20 @@
-//import React, {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import axios from "axios";
 
 const CreateAccount = () => {
 
-    const onUserChange = e => {
-        this.setState({
-          username: e.target.value
-        });
-      };
-    
-     const  onEmailChange = e => {
-        this.setState({
-          email: e.target.value
-        });
-      };
-
-      const onPasswordChange = e => {
-        this.setState({
-          password: e.target.value
-        });
-      };
-    
-      const onPasswordTwoChange = e => {
-        this.setState({
-          passwordCheck: e.target.value
-        });
-      };
-
-   const handleSubmit = e => {
-        e.preventDefault();
-        const data = {
-          username: this.state.username,
-          email: this.state.email,
-          password: this.state.password,
-          passwordCheck: this.state.passwordCheck
-        };
-        axios
-        .post("/sign_up", data)
-        .then(res => console.log(res))
+    const handleSubmit = event => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        axios.post("/sign_up", data,
+        {
+          headers:  {
+            'Content-Type': 'application/json'
+          } 
+        }).then(res => console.log(res))
         .catch(err => console.log(err));
-    }
+    };
 
 
     return(
@@ -48,22 +22,23 @@ const CreateAccount = () => {
             <div className="content">
                 <div className ="createAccountForm">
                     <h1>Create Your Account</h1>
-                    <form onSubmit={this.handleSubmit}>
-                        <input type="text" name="username" placeHolder="Enter Username" value={this.state.username}
-                            onChange={this.onUserChange}/><br /><br />
-                        <input type="text" name="email" placeHolder="Enter an Email" value={this.state.email}
-                            onChange={this.onEmailChange}/><br /><br />                        
-                        <input type="password" name="password" placeHolder="Enter Password" value={this.state.password}
-                            onChange={this.onPasswordChange}/><br /><br />
-                        <input type="password" name="passwordCheck" placeHolder="Re-enter Password" value={this.state.passwordCheck}
-                            onChange={this.onPasswordTwoChange}/><br /><br />                                                                       
-                        <Link to="/"><button type="submit">Submit</button></Link>
+                    <form classname = "post" onSubmit={handleSubmit}>
+                        <input type="text" name="username" placeHolder="Enter Username"
+                            /><br /><br />
+                        <input type="text" name="email" placeHolder="Enter an Email"
+                            /><br /><br />                        
+                        <input type="password" name="password" placeHolder="Enter Password"
+                            /><br /><br />
+                        <input type="password" name="passwordCheck" placeHolder="Re-enter Password"
+                            /><br /><br />                                                                       
+                        <button type="submit">Submit</button>
                     </form>
                 </div><br />               
                 <Link to="/"><button type="submit">Cancel</button></Link>
             </div>
         </div>    
     );
+    
 }
 
 export default CreateAccount;
