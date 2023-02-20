bf142cdc33bf24a27c1a4211e0977d3ce6f24639
azind <fzindah@emich.edu>
2023-02-20 15:55:24 -0500

    fix: overflow error

--- client/src/Rules.js
+++ client/src/Rules.js
@@ -11,7 +11,6 @@ const Rules = () => {
         <div className="App">
             <div className ="content rules">
                 <h1>Game Rules</h1>
-                <form>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p><br />
@@ -22,7 +21,6 @@ const Rules = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p><br />           
                 <Link to="/">Back</Link>           
-                </form>
             </div><br />   
         </div>
       )    
