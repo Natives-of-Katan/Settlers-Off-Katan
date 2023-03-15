fed578a4120bf015ba50e8b20f701992cf7ddcc8
Jonathan Espinosa <jonespinosa89@gmail.com>
2023-03-14 20:44:37 -0400

    players have the ability to see when they are eligible to build settlements, roads, or buy development cards assuming they have the correct number of resources in their inventory. Their inventory is displayed during their turn. A scoreboard has also been implemented, as well as dynamic text showing players what amount they rolled. Building is disabled until players have rolled, but the buttons will show (as disabled) so that they know they will be able to build that turn. Resource amounts are checked at the beginning of each turn for the current player, through the 'onBegin' hook in gamelogic.js

--- client/src/Components/gameLogic.js
+++ client/src/Components/gameLogic.js
@@ -6,31 +6,52 @@ const tileNums = [2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12];
 const rollDice = ({G, playerID}) => {
     const d1 = 1+Math.floor(Math.random() *6);
     const d2 = 1+Math.floor(Math.random() *6);
+    G.players[playerID].diceRoll = d1+d2;
 
-    tileNums.forEach((num) => {
-        if(d1+d2 === num)
-            G.players[playerID].resources[tileResource[num]] +=1;
-    })
-
-    console.log(d1+d2);
+    tileNums.forEach((num, index) => {
+        if(d1+d2 === num && d1+d2!==7) {
+            const resource = tileResource[index];
+            G.players[playerID].resources[resource] +=1;
+        }
+    });
 }
+
+const checkBuildActions = ({G, ctx}) => {
+    const currentPlayer = G.players[ctx.currentPlayer]
+    let resources = currentPlayer.resources;
+    const enoughResources = Object.values(resources).every(value => value >= 1);
+    enoughResources ? currentPlayer.canBuildSettlement = true : currentPlayer.canBuildSettlement = false;
+
+    if(currentPlayer.resources.forest >= 1 && currentPlayer.resources.hill >= 1)
+        currentPlayer.canBuildRoad = true;
+
+    if(currentPlayer.resources.pasture >= 1 && currentPlayer.resources.mountain >= 1 && currentPlayer.resources.grain > 1)
+        currentPlayer.canBuyCard = true;
+  }
 
 export const settlersOffKatan = {
     setup: () => ({
         players: Array(4).fill().map( () => ({
-            points: 0,
+            score: 0,
             resources: {
                 grain: 0,
                 pasture: 0,
                 forest: 0,
-                desert: 0,
                 hill: 0,
                 mountain: 0
             },
+            diceRoll: 0,
+            canBuildSettlement: false,
+            canBuildRoad: false,
+            canBuyCard: false,
             settlements: [],
             cards: []
         })),
     }),
+
+    turn: {
+        onBegin: checkBuildActions
+    },
 
     moves: {
         rollDice
