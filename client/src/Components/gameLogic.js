141c1f367c2f2a1683bff05398122c33635fdf13
Jmeads4 <jmeads4@emich.edu>
2023-03-24 12:50:57 -0400

    style: changed variable names
    Changed the names of the resource variables to match the names of the resources in the game. So grain is now wheat, pasture is now sheep, forest is now wood, hill is now brick, and mountain is now ore.

--- client/src/Components/gameLogic.js
+++ client/src/Components/gameLogic.js
@@ -1,7 +1,7 @@
 
-const tileResource = ["grain", "grain", "grain", "grain", "pasture", "pasture", 
-                          "forest", "pasture", "forest", "desert", "forest", "forest", 
-                          "hill", "hill", "hill", "mountain", "mountain", "mountain", "pasture"];
+const tileResource = ["wheat", "wheat", "wheat", "wheat", "sheep", "sheep", 
+                          "wood", "sheep", "wood", "desert", "wood", "wood", 
+                          "brick", "brick", "brick", "ore", "ore", "ore", "sheep"];
 const tileNums = [2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12];
 
 const rollDice = ({G, playerID, ctx}) => {
@@ -18,11 +18,11 @@ const rollDice = ({G, playerID, ctx}) => {
 }
 
 const addDevelopmentResources = ({G, playerID}) => {
-    G.players[playerID].resources.pasture += 1;
-    G.players[playerID].resources.grain += 1;
-    G.players[playerID].resources.mountain += 1;
-    G.players[playerID].resources.hill += 1;
-    G.players[playerID].resources.forest += 1;
+    G.players[playerID].resources.sheep += 1;
+    G.players[playerID].resources.wheat += 1;
+    G.players[playerID].resources.ore += 1;
+    G.players[playerID].resources.brick += 1;
+    G.players[playerID].resources.wood += 1;
     G.players[playerID].developmentCards.knight += 1;
     G.players[playerID].developmentCards.victory += 1;
     G.players[playerID].developmentCards.monopoly += 1;
@@ -32,15 +32,15 @@ const addDevelopmentResources = ({G, playerID}) => {
 
 const drawDevelopmentCard = ({G, playerID}) => {
     //Checks if the player has the required resources and that the deck has at least 1 card in it
-    if (G.players[playerID].resources.pasture > 0 && G.players[playerID].resources.grain > 0 && G.players[playerID].resources.mountain > 0 && G.deck.knight+G.deck.victory+G.deck.monopoly+G.deck.plenty+G.deck.road > 0) {
+    if (G.players[playerID].resources.sheep > 0 && G.players[playerID].resources.wheat > 0 && G.players[playerID].resources.ore > 0 && G.deck.knight+G.deck.victory+G.deck.monopoly+G.deck.plenty+G.deck.road > 0) {
         
         //Determines what card will be drawn
         const drawnCard = 1+Math.floor(Math.random() * (G.deck.knight+G.deck.victory+G.deck.monopoly+G.deck.plenty+G.deck.road));
         
         //Subtracts the resources from the player
-        G.players[playerID].resources.pasture -= 1;
-        G.players[playerID].resources.grain -= 1;
-        G.players[playerID].resources.mountain -= 1;
+        G.players[playerID].resources.sheep -= 1;
+        G.players[playerID].resources.wheat -= 1;
+        G.players[playerID].resources.ore -= 1;
         
         //Adds card to player's hand and removes it from the deck
         if (drawnCard <= G.deck.knight) {
@@ -80,27 +80,27 @@ const playYearOfPlenty = ({G , playerID}, choice1, choice2) => {
         G.players[playerID].developmentCards.plenty -=1;
         G.players[playerID].canPlayCard = false;
 
-        if (choice1 === 'grain') 
-            G.players[playerID].resources.grain += 1;
-        else if (choice1 === 'pasture') 
-            G.players[playerID].resources.pasture += 1;
-        else if (choice1 === 'forest') 
-            G.players[playerID].resources.forest += 1;
-        else if (choice1 === 'hill') 
-            G.players[playerID].resources.hill += 1;
-        else if (choice1 === 'mountain') 
-            G.players[playerID].resources.mountain += 1;
+        if (choice1 === 'wheat') 
+            G.players[playerID].resources.wheat += 1;
+        else if (choice1 === 'sheep') 
+            G.players[playerID].resources.sheep += 1;
+        else if (choice1 === 'wood') 
+            G.players[playerID].resources.wood += 1;
+        else if (choice1 === 'brick') 
+            G.players[playerID].resources.brick += 1;
+        else if (choice1 === 'ore') 
+            G.players[playerID].resources.ore += 1;
 
-        if (choice2 === 'grain') 
-            G.players[playerID].resources.grain += 1;
-        else if (choice2 === 'pasture') 
-            G.players[playerID].resources.pasture += 1;
-        else if (choice2 === 'forest') 
-            G.players[playerID].resources.forest += 1;
-        else if (choice2 === 'hill') 
-            G.players[playerID].resources.hill += 1;
-        else if (choice2 === 'mountain') 
-            G.players[playerID].resources.mountain += 1; 
+        if (choice2 === 'wheat') 
+            G.players[playerID].resources.wheat += 1;
+        else if (choice2 === 'sheep') 
+            G.players[playerID].resources.sheep += 1;
+        else if (choice2 === 'wood') 
+            G.players[playerID].resources.wood += 1;
+        else if (choice2 === 'brick') 
+            G.players[playerID].resources.brick += 1;
+        else if (choice2 === 'ore') 
+            G.players[playerID].resources.ore += 1; 
     }
 }
 
@@ -113,45 +113,45 @@ const playMonopoly = ({G, playerID}, choice) => {
         let numResource = 0;
         
 
-        if (choice === 'grain') {
+        if (choice === 'wheat') {
             for (let i = 0; i < G.players.length; i++) {
-                numResource += G.players[i].resources.grain;
-                G.players[i].resources.grain = 0;
+                numResource += G.players[i].resources.wheat;
+                G.players[i].resources.wheat = 0;
             }
 
-            G.players[playerID].resources.grain = numResource;
+            G.players[playerID].resources.wheat = numResource;
         }
-        else if (choice === 'pasture') {
+        else if (choice === 'sheep') {
             for (let i = 0; i < G.players.length; i++) {
-                numResource += G.players[i].resources.pasture;
-                G.players[i].resources.pasture = 0;
+                numResource += G.players[i].resources.sheep;
+                G.players[i].resources.sheep = 0;
             }
 
-            G.players[playerID].resources.pasture = numResource;
+            G.players[playerID].resources.sheep = numResource;
         }
-        else if (choice === 'forest') {
+        else if (choice === 'wood') {
             for (let i = 0; i < G.players.length; i++) {
-                numResource += G.players[i].resources.forest;
-                G.players[i].resources.forest = 0;
+                numResource += G.players[i].resources.wood;
+                G.players[i].resources.wood = 0;
             }
 
-            G.players[playerID].resources.forest = numResource;
+            G.players[playerID].resources.wood = numResource;
         }
-        else if (choice === 'hill') {
+        else if (choice === 'brick') {
             for (let i = 0; i < G.players.length; i++) {
-                numResource += G.players[i].resources.hill;
-                G.players[i].resources.hill = 0;
+                numResource += G.players[i].resources.brick;
+                G.players[i].resources.brick = 0;
             }
 
-            G.players[playerID].resources.hill = numResource;
+            G.players[playerID].resources.brick = numResource;
         }
-        else if (choice === 'mountain') {
+        else if (choice === 'ore') {
             for (let i = 0; i < G.players.length; i++) {
-                numResource += G.players[i].resources.mountain;
-                G.players[i].resources.mountain = 0;
+                numResource += G.players[i].resources.ore;
+                G.players[i].resources.ore = 0;
             }
 
-            G.players[playerID].resources.mountain = numResource;
+            G.players[playerID].resources.ore = numResource;
         }
     }
 }
@@ -162,10 +162,10 @@ const checkBuildActions = ({G, ctx}) => {
     const enoughResources = Object.values(resources).every(value => value >= 1);
     enoughResources ? currentPlayer.canBuildSettlement = true : currentPlayer.canBuildSettlement = false;
 
-    if(currentPlayer.resources.forest >= 1 && currentPlayer.resources.hill >= 1)
+    if(currentPlayer.resources.wood >= 1 && currentPlayer.resources.brick >= 1)
         currentPlayer.canBuildRoad = true;
 
-    if(currentPlayer.resources.pasture >= 1 && currentPlayer.resources.mountain >= 1 && currentPlayer.resources.grain > 1)
+    if(currentPlayer.resources.sheep >= 1 && currentPlayer.resources.ore >= 1 && currentPlayer.resources.wheat > 1)
         currentPlayer.canBuyCard = true;
   }
 
@@ -186,11 +186,11 @@ export const settlersOffKatan = numPlayers => ({
         players: Array(numPlayers).fill().map( () => ({
             score: 0,
             resources: {
-                grain: 0,
-                pasture: 0,
-                forest: 0,
-                hill: 0,
-                mountain: 0
+                wheat: 0,
+                sheep: 0,
+                wood: 0,
+                brick: 0,
+                ore: 0
             },
             developmentCards: {
                 knight: 0,
