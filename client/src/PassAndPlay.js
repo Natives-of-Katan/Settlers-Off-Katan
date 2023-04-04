ea46f212cbea63b2ab285812990e2e30d63fe594
wgong9 <illecandra@gmail.com>
2023-04-04 19:32:33 -0400

    included new updates from master

--- client/src/PassAndPlay.js
+++ client/src/PassAndPlay.js
@@ -12,10 +12,10 @@ const PassAndPlay = () => {
     const handleSubmit = (event) => {
         setMultiplayer([0,0]);
         event.preventDefault();
-        if (!isNaN(numPlayers) && numPlayers >= 4 && numPlayers <= 8) {     //if input is a number and a number between 4 to 8,
-            navigate('/Game');                              //go to Game page
-        } else {                                                            //else ask user to enter a number between 4 and 8
-          alert("Please enter a number between 4 and 8");  
+        if (!isNaN(numPlayers) && numPlayers >= 3 && numPlayers <= 4) {     
+            navigate('/Game');                              
+        } else {                                                     
+          alert("Please enter 3 or 4 to represent the number of players.");  
         }
     };
 
