d5d103b15d1a08f495e162a4333cc2fed4a71a40
Weevern <gwvern@gmail.com>
2023-04-24 01:03:42 -0400

    feat: added bank trade functionality, and checks for same resource trading among players as well as conditions for bank trade

--- client/src/Components/gameLogic.js
+++ client/src/Components/gameLogic.js
@@ -441,6 +441,28 @@ const discardCards = ({G, ctx}, totalResources, index) => {
     }
 }
 
+const tradeWithBank = (actualG, ctx, currentPlayerIndex, resourceToTrade, resourceToReceive) => {
+
+    const tradeRatio = 3;
+ 
+    const G = produce(actualG.G, draft => {
+
+        console.log('currentPlayerIndex in tradeWithBank:', currentPlayerIndex);
+        console.log('resourceToTrade in makeTrade:', resourceToTrade);
+        console.log('resourceToReceive in makeTrade:', resourceToReceive);
+
+        if (draft.players[currentPlayerIndex].resources[resourceToTrade] >= tradeRatio) {
+            draft.players[currentPlayerIndex].resources[resourceToTrade] -= tradeRatio;
+            draft.players[currentPlayerIndex].resources[resourceToReceive] += 1;
+          } else {
+            console.error(`Player ${currentPlayerIndex}+1 doesn't have enough ${resourceToTrade} resources to trade.`);
+          }
+
+    });
+    
+    return G;
+}
+
 const makeTrade = (actualG, ctx, currentPlayerIndex, selectedPlayerIndex, tradeResources, wantedResources) => {
     console.log('traderesources in makeTrade:', tradeResources);
     console.log('wantedresources in makeTrade:', wantedResources);
@@ -612,6 +634,11 @@ export const settlersOffKatan = numPlayers => ({
         checkLongestRoad,
         updateDevelopmentCards,
         findUsers,
+        tradeWithBank: {
+            move: tradeWithBank,
+            client: false,
+            redact: true,
+        },
         makeTrade: {
             move: makeTrade,
             client: false,
